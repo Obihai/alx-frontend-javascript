0e7f0372f24b91568b8aa72a2cede4662a1191f7
Obihai <obichi@coseo.com>
2023-06-27 14:18:13 +0100

    Using promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+// 0-promise.js
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const response = 'API response';
+      resolve(response);
+    }, 1000);
+  });
+}
+
+export default getResponseFromAPI;
